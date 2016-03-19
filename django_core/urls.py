bf45ad8ede1d2475af0cd76ea8fb3be2a509db2a
konstantin <gritsus@gmail.com>
2016-03-19 21:33:30 +0500

    serializers moved to tests.serializer, help model added

--- django_core/urls.py
+++ django_core/urls.py
@@ -15,22 +15,54 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from rest_framework import routers, viewsets
-from tests.serializers import CourseSerializer
-from tests.models import Course
+from tests.serializers import CourseSerializer, DocumentSerializer, ThemeSerializer, QuestionSerializer, OptionSerializer, HelpSerializer, CourseHyperlinkSerializer, ThemeHyperlinkSerializer
+from tests.models import Course, Document, Theme, Question, Option, Help
+from rest_framework.response import Response
 
 
 # ViewSets define the view behavior.
 class CourseViewSet(viewsets.ModelViewSet):
+
+    def list(self, request):
+        queryset = Course.objects.all()
+        serializer = CourseHyperlinkSerializer(queryset, many=True, context={'request':request})
+        return Response(serializer.data)
+
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
+
+class DocumentViewSet(viewsets.ModelViewSet):
+    queryset = Document.objects.all()
+    serializer_class = DocumentSerializer
+
+
+class ThemeViewSet(viewsets.ModelViewSet):
+
+    def list(self, request):
+        queryset = Theme.objects.all()
+        serializer = ThemeHyperlinkSerializer(queryset, many=True, context={'request':request})
+        return Response(serializer.data)
+
+    queryset = Theme.objects.all()
+    serializer_class = ThemeSerializer
+
+
+class QuestionViewSet(viewsets.ModelViewSet):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
 
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'courses', CourseViewSet)
+router.register(r'documents', DocumentViewSet)
+router.register(r'themes', ThemeViewSet)
+router.register(r'questions', QuestionViewSet)
+
 
 urlpatterns = [
-    url(r'^', include(router.urls)),
+    url(r'^api/', include(router.urls)),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^api/', include('rest_framework.urls', namespace='rest_framework'))
+    #url(r'^api/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- tests/serializers.py
+++ tests/serializers.py
@@ -3,47 +3,107 @@ from rest_framework import routers, serializers, viewsets
 
 
 # Serializers define the API representation.
-class OptionSerializer(serializers.HyperlinkedModelSerializer):
+class OptionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Option
         fields = ('right', 'text')
 
 
-class HelpSerializer(serializers.HyperlinkedModelSerializer):
+class HelpSerializer(serializers.ModelSerializer):
     class Meta:
         model = Help
         fields = ('name', 'content')
 
 
-class QuestionSerializer(serializers.HyperlinkedModelSerializer):
+class QuestionHyperlinkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = ('url', 'text')
+
+
+class QuestionSerializer(serializers.ModelSerializer):
     options = OptionSerializer(many=True)
     # TODO replace many value to false (seems like many=false require any help for question)
     help = HelpSerializer(many=True)
 
     class Meta:
         model = Question
-        fields = ('text', 'help', 'options')
+        fields = ('id', 'text', 'help', 'options',('theme'))
+
+    def create(self, validated_data):
+        if len(validated_data['options']) < 2:
+            raise serializers.ValidationError('you need to send at least 2 options for question')
+        """
+        same_questions = Question.objects.filter(text=validated_data['text'])
+
+        if len(same_questions) > 0:
+            for same_question in same_questions:
+                raise serializers.ValidationError({
+                    'text' : 'you can not add same question in system, please update existing',
+                    'id' : same_question.id})
+        """
+        print(validated_data)
+        if ('theme' in validated_data):
+            question = Question.objects.create(text=validated_data['text'], theme=validated_data['theme'])
+        else:
+            raise serializers.ValidationError({ 'error':'question should be part of theme',
+                                                'text': validated_data['text']})
+
+        for option_data in validated_data['options']:
+            serializer = OptionSerializer(data=option_data)
+            if not serializer.is_valid():
+                raise serializers.ValidationError(serializer.errors)
+
+            option = Option.objects.create(text=option_data['text'], right=option_data['right'], question=question)
+
+        return question
 
 
-class ThemeSerializer(serializers.HyperlinkedModelSerializer):
+class ThemeHyperlinkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Theme
+        fields = ('name', 'url')
+
+
+class ThemeSerializer(serializers.ModelSerializer):
     questions = QuestionSerializer(many=True)
 
     class Meta:
         model = Theme
-        fields = ('name', 'questions')
+        fields = ('name', 'course', 'questions')
 
+    def create(self, validated_data):
+        serializers.ValidationError('fuck yoyu')
+        theme = Theme.objects.create(name=validated_data['name'], course=validated_data['course'])
+        for question_data in validated_data['questions']:
+            print(question_data)
+            question_data['theme'] = 1
+            print(question_data)
+            serializer = QuestionSerializer(data=question_data)
+            if serializer.is_valid():
+                serializer.save()
+            else:
+                serializers.ValidationError('=(')
+        return theme
 
-class DocumentSerializer(serializers.HyperlinkedModelSerializer):
+class DocumentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Document
         fields = ('name', 'content')
 
 
-class CourseSerializer(serializers.HyperlinkedModelSerializer):
-    themes = ThemeSerializer(many=True)
+class CourseHyperlinkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = ('name', 'url')
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    themes = ThemeHyperlinkSerializer(many=True)
     documents = DocumentSerializer(many=True)
 
     class Meta:
         model = Course
         fields = ('name', 'documents', 'themes')
+        depth = 1
 
